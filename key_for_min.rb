d8bf2c25d18f2bc2eb684a724c5aa25a6370e0e3
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-20 23:17:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value_key = " "
 
   if name_hash.empty?
-    min_value_key = nil
+    return nil
   else
     name_hash.each do |key, value|
       if min_value == " " || value < min_value
