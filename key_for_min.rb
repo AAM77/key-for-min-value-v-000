cf2c82c3e4301de092b42332b1f8dbbfea7c67de
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-20 23:15:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,8 @@ def key_for_min_value(name_hash)
   else
     name_hash.each do |key, value|
       if min_value == " " || value < min_value
-        min_value = v
-        min_value_key = k
+        min_value = value
+        min_value_key = key
       end
     end
   min_vlaue_key
