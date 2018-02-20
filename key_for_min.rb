69e42e4f05b79cbb568c61472bdfa078c2be0208
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-20 23:23:54 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 def key_for_min_value(hash)
   lowest_key = nil
   lowest_value = nil
-  hash.each do |k, v|
+  hash.each do |key, value|
     if lowest_value == nil || v < lowest_value
       lowest_value = v
       lowest_key = k
